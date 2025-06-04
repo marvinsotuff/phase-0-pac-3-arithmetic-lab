3da26add1960689d5588b47ff848013c1e3d8fe1
Marvin Kioko <kiokomarvin7@gmail.com>
2025-06-04 14:10:33 +0300

    Adds index.js

---
+++ index.js
@@ -0,0 +1,44 @@
+let a = 10;
+let b = 5;
+
+
+function add(a,b) {
+    return a + b;
+    
+}
+
+function subtract(a,b) {
+    return a - b;
+
+}
+
+function multiply(a,b) {
+    return a * b;
+
+}
+
+function divide(a,b) {
+    return a / b;
+
+}
+
+//Increment and decrement functions
+ let n = 5
+
+function increment(n) {
+    return n + 1;    
+}
+
+function decrement(n) {
+    return n - 1;    
+}
+
+//parseInt function
+
+function makeInt(string) {
+    return parseInt(string, 10);
+}
+
+function preserveDecimal(string) {
+    return parseFloat(string);
+}
